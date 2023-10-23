679cbcf5c9de5a052f0149320e915bd68781ccb3
choco5732 <choco5732@gmail.com>
2023-10-23 18:46:51 +0900

    [REFACTOR] : 중복된 gradle 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.8.1")
-    implementation("com.google.firebase:firebase-storage:20.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -63,29 +61,28 @@ dependencies {
     // okhttp3
     implementation("com.squareup.okhttp3:logging-interceptor:4.5.0")
     // firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.4.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
-    //Coil
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-firestore")
+    // Coil
     implementation("io.coil-kt:coil:0.10.0")
-    //ViewModel
+    // ViewModel
     implementation("androidx.activity:activity-ktx:1.8.0")
     implementation("androidx.fragment:fragment-ktx:1.3.6")
-    implementation("com.google.firebase:firebase-database-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")
-
+    // muddz
     implementation("io.github.muddz:styleabletoast:2.4.0")
-
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
-
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.1")
+    // swiperfreshlayout
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     // Also add the dependency for the Google Play services library and specify its version
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.gms:google-services:4.3.15")
-    implementation("com.google.firebase:firebase-auth:22.0.0")
-    implementation("com.google.firebase:firebase-bom:32.0.0")
-
+    // glide
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 }
